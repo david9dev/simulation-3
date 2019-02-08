6d367d6274031e27b1d414b663a9e1c792b04f01
David Wells <davidwells@Davids-MacBook-Pro.local>
2019-02-08 15:39:18 -0700

    post still needs the axios request to get its data

--- sim3/db/messages/get_post_by_id.sql
+++ sim3/db/messages/get_post_by_id.sql
@@ -1,2 +1,4 @@
-select * from posts
+select title, img, content u.username 
+from posts as p
+join users as u on p.author_id = u.id
 where id = $1;
--- sim3/src/components/Post/Post.js
+++ sim3/src/components/Post/Post.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Post extends Component
 {
@@ -6,6 +7,10 @@ class Post extends Component
     {
         super();
         this.state = {};
+    }
+    componentDidMount()
+    {
+
     }
     render()
     {
